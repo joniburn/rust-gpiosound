cc7b8fb1ddfed39f8b70658ab2cdff4cda8736f6
joniburn <joniburn@gmail.com>
2021-10-03 16:57:53 +0900

    structoptで引数パースを実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(short, long, default_value = "50")]
+    duty: u8,
+    #[structopt(subcommand)]
+    out_type: OutType,
+}
+
+#[derive(Debug, StructOpt)]
+enum OutType {
+    Tone { pin: u8 },
+    Noise { pin: u8 },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    let pin = match args.out_type {
+        OutType::Tone { pin } => pin,
+        OutType::Noise { pin } => pin,
+    };
+    println!(
+        "duty={}, out_type={:?}, pin={}",
+        args.duty, args.out_type, pin
+    );
 }
